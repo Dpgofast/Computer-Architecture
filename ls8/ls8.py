3dcc79a9cde0b200d8d8a84e9370eebaaec2b3d2
Dpgofast <43558343+Dpgofast@users.noreply.github.com>
2020-01-31 13:10:21 -0600

    SC Complete with stretch

--- ls8/ls8.py
+++ ls8/ls8.py
@@ -5,7 +5,21 @@
 import sys
 from cpu import *
 
-cpu = CPU()
 
-cpu.load()
-cpu.run()
+def main(argv):
+    """Main."""
+
+    if len(argv) != 2:
+        print(f"usage: {argv[0]} filename", file=sys.stderr)
+        return 1
+
+    cpu = CPU()
+
+    cpu.load(argv[1])
+    cpu.run()
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
